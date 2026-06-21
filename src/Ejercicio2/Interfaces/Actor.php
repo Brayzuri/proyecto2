2a040c01549ea374d9d0fb7a7a8ab89b1b4c5730
Brayzuri <158206835+Brayzuri@users.noreply.github.com>
2026-06-21 00:56:58 -0400

    Delete src directory

--- src/Ejercicio2/Interfaces/Actor.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-
-interface Actor {
-    public function realizarAccion(): string;
-}
--- src/ejercicio1/Felino.php
+++
@@ -1,4 +0,0 @@
-<?php
-declare(strict_types=1);
-
-abstract class Felino extends Animal {}
